ed90965ac5bd6d809065dc833a287bac8fd22a59
zhongpengGao <71818776+zhongpengGao@users.noreply.github.com>
2020-10-04 17:36:15 +0800

    Add files via upload

---
+++ OddAndEven.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package homeworkexercises;
+
+/**
+ * @author asus
+ *
+ */
+public class OddAndEven {
+     double randomNumber;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       double randomNumber=Math.random();
+       int number=(int)(randomNumber*10+1);
+       if(number%2==0)
+    	   System.out.println("The random number is even!");
+       else
+    	   System.out.println("The random number is Odd!");
+	}
+
+}
